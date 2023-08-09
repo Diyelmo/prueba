f23973b2ae68285527b2ae0fd6b3a3a5ec09b942
Juan Castro <juanboffi44@gmail.com>
2023-08-09 16:22:53 -0300

    prueba de rama
    agregado un nuevo proyecto netbeans a una rama de prueba

---
+++ prueba rama 1/src/prueba/rama/pkg1/PruebaRama1.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package prueba.rama.pkg1;
+
+/**
+ *
+ * @author User
+ */
+public class PruebaRama1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("hOLA, ESTO ES UNA PRUEBA");
+        System.out.println("Rama 1");
+    }
+    
+}
